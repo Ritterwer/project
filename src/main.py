32ed9350528ac8ef67881a4ebac2a1da87c1cee0
andrey kravchenko <fullout777@mail.ru>
2024-10-10 20:36:15 +0600

    коммит инициализации

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+get_mask_card_number('7000792289606361')
+get_mask_account('73654108430135874305')
---
+++ src/masks.py
@@ -0,0 +1,18 @@
+account_num = input()
+card_number_str = input()
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая маскирует номер карты"""
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция, которая маскирует номер счета"""
+
+    return f"**{account_number[-4:]}"
+
+
+print(get_mask_card_number('7000792289606361'))
+print(get_mask_account('73654108430135874305'))
