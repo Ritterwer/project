4b285893aaf2e01f776b42e78f35a67555d41bde
andrey kravchenko <fullout777@mail.ru>
2024-10-18 23:29:43 +0600

    change_homework_type

--- src/processing.py
+++ src/processing.py
@@ -8,7 +8,7 @@ my_list_dict = [
 ]
 
 
-def filter_by_state(my_list_dict: list[dict[str, Any]], state: str = "EXECUTED") -> list:
+def filter_by_state(my_list_dict: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция принимает список словарей и значение для ключа и возвращает новый
     список содержащий только те словари у которых ключ содержит переданное значение"""
 
