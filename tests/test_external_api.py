8f4182fd1e7fac8b94c5e7e6367f842b396415bf
andrey kravchenko <fullout777@mail.ru>
2024-12-12 03:57:49 +0600

    home_work_12_1 добавление папок utils и external_api, а так же тестов к ним

---
+++ tests/test_external_api.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import patch
+
+from src.external_api import currency_conversion
+
+
+class TestCurrencyConversion(unittest.TestCase):
+    @patch("requests.get")
+    def test_conversion_valid(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {"result": 7500.0}
+        transaction = {"operationAmount": {"amount": 100, "currency": {"code": "USD"}}}
+        result = currency_conversion(transaction)
+        self.assertEqual(result, 7500.0)
+
+    def test_conversion_invalid(self):
+        transaction_info = {
+            "operationAmount": {"amount": None, "currency": {"code": None}}
+        }
+        with self.assertRaises(ValueError):
+            currency_conversion(transaction_info)
